eb2d2b9d6855799954b2d99329a542267bd22cc4
raja <rdas828726@gmail.com>
2024-03-04 21:21:43 +0530

    init

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors=require('cors');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+/*
 app.get('/',async(req,resp)=>{
     let data= await dm.find();
     console.log("this:",req.body)
@@ -23,7 +23,7 @@ app.post('/',async(req,resp)=>{
 
     resp.send(`done ${data}`);
 
-})
+})*/
 
 
 
